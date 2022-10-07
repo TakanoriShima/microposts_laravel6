66bcad1740809dd5cccb9ad411a4a9340f04dd32
ryutaro.oshiro <r.oshiro@lexues.co.jp>
2022-10-07 07:37:36 +0000

    create: top page

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,9 @@
+{{-- エラーメッセージの表示 --}}
+
+@if (count($errors) > 0)
+    <ul class="alert alert-danger" role="alert">
+        @foreach ($errors->all() as $error)
+            <li class="mb-4">{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
