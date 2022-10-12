3f472872ea808f14dcd0383aa253bcd337308530
ryutaro.oshiro <r.oshiro@lexues.co.jp>
2022-10-12 07:15:35 +0000

    user registration

--- app/User.php
+++ app/User.php
@@ -14,6 +14,9 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array
+     * 「一気に保存可能」なパラメータの指定
+     * create()を使ってデータを保存する時には、Modelファイル内に$fillableを定義
+     * create()で保存可能なパラメータを配列として代入しておく必要がある。
      */
     protected $fillable = [
         'name', 'email', 'password',
@@ -23,6 +26,7 @@ class User extends Authenticatable
      * The attributes that should be hidden for arrays.
      *
      * @var array
+     * パスワードなどの秘匿しておきたいカラムを隠してくれる
      */
     protected $hidden = [
         'password', 'remember_token',
@@ -32,6 +36,7 @@ class User extends Authenticatable
      * The attributes that should be cast to native types.
      *
      * @var array
+     * 
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,10 @@
 @section("content")
     <div class="center jumbotron">
         <div class="text-center">
-            <H1>Welcome to the Microsposts</H1>
+            <H1>Welcome to the Microposts</H1>
+            
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
         </div>
     </div>
 
