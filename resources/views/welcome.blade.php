4f808a02cc1bc007d1b1bb50070969b6ded42546
ryutaro.oshiro <r.oshiro@lexues.co.jp>
2022-10-12 08:33:01 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,18 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="center jumbotron">
-        <div class="text-center">
-            <H1>Welcome to the Microposts</H1>
-            
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
+    @if (Auth::check())
+        <div class="center jumbotron">
+            <div class="text-center">
+                <H1>Welcome to {{ Auth::user()->name }} !!</H1>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <H1>Welcome to the Microposts !</H1>
+                
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
+            </div>
         </div>
-    </div>
-
+    @endif
 @endsection()
