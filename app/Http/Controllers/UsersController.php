1de24709d0d5e4c3db2d2a53d4bc0d32158b259a
ryutaro.oshiro <r.oshiro@lexues.co.jp>
2022-10-17 08:59:07 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,10 +24,17 @@ class UsersController extends Controller
     public function show($id){
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
+        
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+        
+        // ユーザの投稿一覧を作成日時順の降順で取得
+        $microposts =$user->microposts()->orderBy("created_at", "desc")->paginate(10);
 
         // ユーザ詳細ビューでそれを表示
         return view("users.show", [
             "user" => $user,
+            "microposts" => $microposts,
         ]);
     }
     
