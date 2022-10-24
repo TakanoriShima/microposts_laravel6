2fce1ea7b13f945a8fd32d0759dd8ab4c822bc94
ryutaro.oshiro <r.oshiro@lexues.co.jp>
2022-10-24 08:05:04 +0000

    timeline

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,25 +3,28 @@
 @section("content")
     <div class="row">
         <aside class="col-sm-4">
+            
             {{-- ユーザ情報 --}}
             @include("users.card")
+            
+            {{-- ユーザ削除フォーム --}}
+            @if (Auth::id() == $user->id)
+                {!! Form::model($user, ["route" => ["users.destroy", $user->id], "method" => "delete"]) !!}
+                    {!! Form::submit("削除", ["class" => "btn btn-danger"]) !!}
+                {!! Form::close() !!}
+            @endif
         </aside>
         <div class="col-sm-8">
             {{-- タブ --}}
             @include("users.navtabs")
+            
+            {{-- 投稿フォーム --}}
             @if (Auth::id() == $user->id)
-                {{-- 投稿フォーム --}}
                 @include("microposts.form")
             @endif
-                {{-- 投稿一覧 --}}
-                @include("microposts.microposts")
+            
+            {{-- 投稿一覧 --}}
+            @include("microposts.microposts")
         </div>
     </div>
-    
-    @if (Auth::id() == $user->id)
-        {{-- ユーザ削除フォーム --}}
-        {!! Form::model($user, ["route" => ["users.destroy", $user->id], "method" => "delete"]) !!}
-            {!! Form::submit("削除", ["class" => "btn btn-danger"]) !!}
-        {!! Form::close() !!}
-    @endif
 @endsection
